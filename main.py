72af2abfc08861cf42f26f982f5596abe8524c28
MrValdez <s_cloudx@yahoo.com>
2012-09-20 13:09:04 +0800

    added clicking

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ pygame.mouse.set_visible(True)
 
 screen = pygame.display.set_mode([640,480])
 clock = pygame.time.Clock()
+font = pygame.font.Font("freesansbold.ttf", 32)
 
 GameRunning = True
 
+mouse_pressed = pygame.mouse.get_pressed()    
+
 while GameRunning:
+    screen.fill(pygame.Color(0,0,0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -20,7 +25,13 @@ while GameRunning:
             pygame.quit()
             GameRunning = False
 
+    prev_mouse_pressed = mouse_pressed
     mouse_pos = pygame.mouse.get_pos()
+    mouse_pressed = pygame.mouse.get_pressed()
+
+    if mouse_pressed[0] and not prev_mouse_pressed[0]:
+        text = font.render("test", False, pygame.Color(255, 255, 255))
+        screen.blit(text, [0,0])
 
     pygame.display.update()
     clock.tick(60)
