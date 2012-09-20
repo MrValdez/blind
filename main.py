0b83eb07d2669c1aad577c39f1b02b0a174e6a4c
MrValdez <s_cloudx@yahoo.com>
2012-09-20 13:18:24 +0800

    added circle pings

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ GameRunning = True
 
 mouse_pressed = pygame.mouse.get_pressed()    
 
+class CirclePing:
+    def __init__(self, pos):
+        self.pos = pos
+        self.radius = 0
+
+    def isAlive(self):
+        if self.radius > 300:
+            return False
+        return True
+
+    def update(self):
+        self.radius += 1
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, pygame.Color(128, 128, 128), self.pos, self.radius)
+
+CirclePingCollection = []
+
 while GameRunning:
     screen.fill(pygame.Color(0,0,0))
     
@@ -32,6 +50,15 @@ while GameRunning:
     if mouse_pressed[0] and not prev_mouse_pressed[0]:
         text = font.render("test", False, pygame.Color(255, 255, 255))
         screen.blit(text, [0,0])
+        CirclePingCollection.append(CirclePing(mouse_pos))
+
+    for Circle in CirclePingCollection:
+        Circle.update()
+        Circle.draw(screen)
+
+    for Circle in CirclePingCollection:
+        if not Circle.isAlive():
+            CirclePingCollection.remove(Circle)
 
     pygame.display.update()
     clock.tick(60)
